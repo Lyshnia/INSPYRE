da6bd7769eb6efe6607dc9bc045f11ca105d009f
Jerry U <jerry@lyshnia.com>
2018-05-10 11:24:25 +0800

    Added Spring Security Rest

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import inspyre.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener)
 }
--- grails-app/init/inspyre/BootStrap.groovy
+++ grails-app/init/inspyre/BootStrap.groovy
@@ -3,6 +3,13 @@ package inspyre
 class BootStrap {
 
     def init = { servletContext ->
+        def role1 = new Role(authority: "ROLE_ADMIN").save flush: true
+        def user1 = new User(username: "styl3r9", password: "pass").save flush: true
+        UserRole.create(user1, role1)
+
+        def role2 = new Role(authority: "ROLE_USER").save flush: true
+        def user2 = new User(username: "styl3r", password: "pass").save flush: true
+        UserRole.create(user2, role2)
     }
     def destroy = {
     }
